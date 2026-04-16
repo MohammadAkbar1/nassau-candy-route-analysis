3b0512d21d46445fcadbf41a58c6260c1dd53892
MohammadAkbar1 <mohammadakbar5450@gmail.com>
2026-04-16 18:20:31 +0530

    Fix plotly layout error

--- Dashboard.py
+++ Dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import pandas as pd
+import pandas as pd 
+df = pd.read_csv('data/dataset.csv')
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
